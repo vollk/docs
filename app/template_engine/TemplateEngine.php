1ef7e9f129dee731f5cda4799ad4955b79368da9
vollk <ovkasatkin@mail.ru>
2015-05-21 16:32:23 +0500

    add templateEngine

---
+++ app/template_engine/TemplateEngine.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * User: kasatkin.o.v
+ * Date: 21.05.2015
+ * Time: 12:38
+ */
+
+namespace vollk;
+
+include_once dirname(__FILE__)."/lib/phpexcel/PHPExcel.php";
+
+class TemplateEngine {
+
+    private $doc_data;
+    private $template_path;
+    private $dest_path;
+
+    public function __construct(array $doc_data, $template_path , $dest_path)
+    {
+        $this->doc_data = $doc_data;
+        $this->template_path = $template_path;
+        $this->dest_path = $dest_path;
+    }
+
+    public function compile()
+    {
+        if(!is_file($this->template_path))
+        {
+            throw new \Exception('template not found');
+        }
+
+
+        $objPHPExcel = \PHPExcel_IOFactory::load($this->template_path);
+        $objPHPExcel->setActiveSheetIndex(0);
+        $aSheet = $objPHPExcel->getActiveSheet();
+        for($row_number=1;$row_number < 40;$row_number++)
+        {
+            for($col_number=0;$col_number < 40;$col_number++)
+            {
+                $this->compileCellVal($aSheet, $col_number, $row_number);
+            }
+        }
+
+        $objWriter = new \PHPExcel_Writer_Excel5($objPHPExcel);
+        $objWriter->save($this->dest_path);
+    }
+
+    private function compileCellVal($aSheet, $col_number, $row_number)
+    {
+        $need_replace = 0;
+        $new_words = array();
+        $val = trim($aSheet->getCellByColumnAndRow($col_number, $row_number)->getValue());
+        $words = explode(' ',$val);
+
+        array_map(function($elem){
+            return trim($elem);
+        }, $words);
+
+        foreach($words as $word)
+        {
+            if(mb_substr($word,0,1) == '%')
+            {
+                $need_replace = 1;
+                $param_name = mb_substr($word, 1);
+                if(isset($this->doc_data[$param_name]))
+                    $new_words[] = $this->doc_data[$param_name];
+                else
+                    $new_words[] = '';
+            }
+            else
+                $new_words[] = $word;
+        }
+        $new_val = implode(' ',$new_words);
+
+        if($need_replace)
+            $aSheet->setCellValueByColumnAndRow($col_number, $row_number,$new_val);
+    }
+} 
---
+++ app/template_engine/lib/phpexcel/readme.txt
@@ -0,0 +1,1 @@
+put phpexcel in this library
