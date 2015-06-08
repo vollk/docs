1922fa69140eacdb8d37e701107a268419dc30d1
vollk <ovkasatkin@mail.ru>
2015-06-08 23:53:22 +0600

    init

---
+++ app/DateUtils.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Vollk
+ * Date: 08/06/15
+ * Time: 09:51
+ */
+
+class DateUtils {
+    private static $rod_monthes = [
+        'января',
+        'февраля',
+        'марта',
+        'апреля',
+        'мая',
+        'июня',
+        'июля',
+        'августа',
+        'сентября',
+        'октября',
+        'ноября',
+        'декабря',
+    ];
+
+    // из дд.мм.гггг делаем гггг-мм-дд
+    static function make_date($string)
+    {
+        if ($string)
+        {
+            $parts = explode('.',$string);
+            return implode('-',array_reverse($parts));
+        }
+        return false;
+    }
+
+    // из гггг-мм-дд делаем дд.мм.гггг
+    static function make_date_to_output($string)
+    {
+        if ($string)
+        {
+            $parts = explode('-',$string);
+            $month_index = (int)$parts[1] -1;
+            return $parts[2].' '.self::$rod_monthes[$month_index].' '.$parts[0];
+        }
+        return false;
+    }
+}
