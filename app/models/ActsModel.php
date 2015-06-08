46008a4836605483d7af5690dae156634e593fa8
vollk <ovkasatkin@mail.ru>
2015-06-08 23:52:19 +0600

    complete print acts

--- app/models/ActsModel.php
+++ app/models/ActsModel.php
@@ -9,11 +9,14 @@ class ActsModel
            throw new Exception('act '.$id.' not found');
         }
         $campData = $this->getCampData($id);
+        $this->prepareData($act_data);
 
-        $file_name = Application::$docsPath.DIRECTORY_SEPARATOR.'act1.xls';
+        $file_name = Application::$docsPath.DIRECTORY_SEPARATOR.$this->get_file_name($act_data);
         $template = Application::$templatePath.DIRECTORY_SEPARATOR.'default'.DIRECTORY_SEPARATOR.'act.xls';
         //throw new Exception($template);
-        $engine = new TemplateEngine(array_merge($act_data,$campData), $template, $file_name);
+        $engine = new TemplateEngine(array_merge(
+            $act_data,
+            $campData), $template, $file_name);
         $engine->compile();
         return $file_name;
     }
@@ -22,6 +25,9 @@ class ActsModel
     {
         $application = Application::getInstance();
         $db = $application['db'];
+        /**
+         * @var \Doctrine\DBAL\Connection $db
+         */
         $sql = <<<q
 select a.id,date,number,sum,`desc`,
 p.name as partner,p.inn as partner_inn,p.kpp as partner_kpp,p.address as partner_address,
@@ -37,9 +43,28 @@ q;
     private function getCampData()
     {
         $application = Application::getInstance();
-        $paramManager = $application['ParamManager'];
+        $paramManager = $application['paramManager'];
         return [
-        'org_acc'=>$paramManager->getParam('org_acc')];
+        'org_inn'=>$paramManager->getParam('org_inn'),
+        'org_kpp'=>$paramManager->getParam('org_kpp'),
+        'org_acc'=>$paramManager->getParam('org_acc'),
+        'org_address'=>$paramManager->getParam('org_address'),
+        'org_kor_acc'=>$paramManager->getParam('org_kor_acc'),
+        'org_bank'=>$paramManager->getParam('org_bank'),
+        'org_bik'=>$paramManager->getParam('org_bik'),
+        'org_phone'=>$paramManager->getParam('org_phone'),
+        'organization'=>$paramManager->getParam('organization'),
+        ];
     }
 
+    private function prepareData(array & $data)
+    {
+        $data['sum_prop'] = MoneyUtils::num2str($data['sum']);
+        $data['sum'] = ' '.$data['sum'];
+        $data['date'] = DateUtils::make_date_to_output($data['date']);
+    }
+    private function get_file_name($act_data)
+    {
+        return 'act '.$act_data['number'].'.xls';
+    }
 }
