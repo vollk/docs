8f59e8eed1b94802f72d4263898295a3af8e07ed
vollk <ovkasatkin@mail.ru>
2019-02-02 23:29:16 +0500

    some old changes

--- html/js/app/myCtrl.js
+++ html/js/app/myCtrl.js
@@ -1,13 +1,23 @@
-var app = angular.module("app", []);
-app.controller("ctrl", function($scope, $http, $timeout,$filter) {
+var app = angular.module("app", ["ngRoute"]);
+app.config(function($routeProvider,$locationProvider) {
+    $routeProvider
+        .when("/acts", {
+            templateUrl : "templates/actsGrid.html"
+        })
+        .when("/bills", {
+            templateUrl : "templates/billsGrid.html"
+        })
+});
+app.controller("ctrl", function($scope, $http, $timeout,$filter,$location) {
+    $scope.$on('$viewContentLoaded', function(a,b,c) {
+        $scope.updateRecords()}
+    );
 
     $scope.orderBy = '';
     $scope.direction = '';
+    $scope.currentTpl = '';
 
     var orderBy = $filter('orderBy');
-    $scope.filterGrid = function($event) {
-        $timeout(updateRecords,500);
-    };
 
     $scope.sortGrid = function(field) {
         $scope.orderBy = field;
@@ -15,34 +25,20 @@ app.controller("ctrl", function($scope, $http, $timeout,$filter) {
         $scope.records = orderBy($scope.records,field,$scope.direction);
     };
 
-    var updateRecords = function()
+
+    $scope.updateRecords = function()
     {
         var params = {};
-        var filters = serializeFilters();
-        if(!isEmpty(filters)) params.filters = filters;
-
-        $http.get("acts",{params:params})
+        var path = $location.path();
+        var gridName = path.substring(1);
+        $http.get(gridName,{params:params})
             .then(function(response) {
                 $scope.records = response.data.records;
             });
     };
 
-    var serializeFilters = function()
-    {
-        var res = {};
-        for (filter in $scope.filters)
-        {
-            if($scope.filters[filter].length > 0)
-            {
-                res[filter] = $scope.filters[filter];
-            }
-        }
-        return res;
-    };
 
     var isEmpty = function(obj) {
         return Object.keys(obj).length === 0;
     };
-
-    updateRecords();
 });
